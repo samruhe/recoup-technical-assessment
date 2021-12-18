7ab93adfd0bc3e34d0ff8fb15663fe98cebf27fe
Sam Ruhe <sar420@lehigh.edu>
2021-12-17 21:02:49 -0800

    handled logging in an existing user

--- react/Components/Auth/Login.js
+++ react/Components/Auth/Login.js
@@ -1,45 +1,65 @@
 import React, { Component } from 'react'
 import { SafeAreaView, StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import firebase from '@react-native-firebase/app';
 
 class Login extends Component {
     state = {
-        username: '',
         email: '',
         password: '',
         errorMessage: null
     };
     
     handleLogin = () => {
-        // TODO: Firebase stuff...
-        console.log('handleLogin')
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(this.state.email.trim(), this.state.password.trim())
+            .then(() => this.props.navigation.navigate('Main'))
+            .catch(error => {
+                if (error.code == 'auth/wrong-password')
+                    this.setState({ errorMessage: "Incorrect email/password" });
+                else if (error.code = 'auth/invalid-email')
+                    this.setState({ errorMessage: "Email address is invalid" });
+                else if (error.code == 'auth/user-disabled')
+                    this.setState({ errorMessage: "This account has been disabled" });
+                else if (error.code == 'auth/user-not-found')
+                    this.setState({ errorMessage: "There is no account matching this email" });
+            });
     }
 
     render() {
         return (
             <SafeAreaView style={styles.container}>
-                <Text style={styles.header}>Login</Text>
-                {this.state.errorMessage &&
-                    <Text style={{ color: 'red' }}>
-                        {this.state.errorMessage}
-                    </Text>}
-                <TextInput
-                    autoCapitalize="none"
-                    placeholder="Email"
-                    onChangeText={email => this.setState({ email })}
-                    value={this.state.email}
-                />
-                <TextInput
-                    secureTextEntry
-                    autoCapitalize="none"
-                    placeholder="Password"
-                    onChangeText={password => this.setState({ password })}
-                    value={this.state.password}
-                />
-                <Button title="Login" onPress={this.handleLogin} />
-                <Button
-                    title="Don't have an account? Sign Up"
-                    onPress={() => this.props.navigation.navigate('SignUp')}
-                />
+                <View style={styles.subContainer}>
+                    <Text style={styles.header}>Login</Text>
+                    <View style={styles.errorContainer}>
+                        {this.state.errorMessage &&
+                            <Text style={{ color: 'red' }}>
+                                {this.state.errorMessage}
+                            </Text>}
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.textInput}
+                            autoCapitalize="none"
+                            placeholder="Email"
+                            onChangeText={email => this.setState({ email })}
+                            value={this.state.email}
+                        />
+                        <TextInput
+                            style={styles.textInput}
+                            secureTextEntry
+                            autoCapitalize="none"
+                            placeholder="Password"
+                            onChangeText={password => this.setState({ password })}
+                            value={this.state.password}
+                        />
+                    </View>
+                    <Button title="Login" onPress={this.handleLogin} />
+                    <Button
+                        title="Don't have an account? Sign Up"
+                        onPress={() => this.props.navigation.navigate('SignUp')}
+                    />
+                </View>
             </SafeAreaView>
         );
     }
@@ -49,8 +69,27 @@ const styles = StyleSheet.create({
         flex: 1,
         alignContent: 'center'
     },
+    errorContainer: {
+        height: 50,
+        marginTop: 20
+    },
     header: {
-        alignSelf: 'center'
+        alignSelf: 'center',
+        fontSize: 30
+    },
+    inputContainer: {
+        width: '90%',
+        height: 90,
+        justifyContent: 'space-between'
+    },
+    subContainer: {
+        marginTop: 50,
+        alignItems: 'center'
+    },
+    textInput: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'gray',
+        height: 40,
     }
 });
 
